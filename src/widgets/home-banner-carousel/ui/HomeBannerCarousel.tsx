3f2d0780d52cf058dffab1e0e51adea4ad5bc8de
Cennge <ilyagliba02@gmail.com>
2025-12-29 01:00:38 +0200

    banner swiper

--- src/widgets/home-banner-carousel/ui/HomeBannerCarousel.tsx
+++ src/widgets/home-banner-carousel/ui/HomeBannerCarousel.tsx
@@ -1,8 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, EffectFade } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-fade';
+import 'swiper/swiper-bundle.css';
 import './HomeBannerCarousel.css';
 
 import banner1 from '@shared/assets/images/homePage/prake-1.png';
