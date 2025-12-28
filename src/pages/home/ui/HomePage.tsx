91ba2db9847948b2fdf8ce234e6ee23ff32f9120
Cennge <ilyagliba02@gmail.com>
2025-12-28 23:56:06 +0200

    banner

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,6 +1,14 @@
+import './HomePage.css';
+import homeBanner from '@shared/assets/images/homePage/prake-1.png';
+import homeBannerMobile from '@shared/assets/images/homePage/prake-1-mobile.png';
+
 export const HomePage = () => {
     return (
         <div>
+            <picture>
+                <source media="(max-width: 768px)" srcSet={homeBannerMobile} />
+                <img src={homeBanner} alt="Banner" className="home-banner" />
+            </picture>
             <h1>Home Page</h1>
             <h1>Home Page</h1>
             <h1>Home Page</h1>
